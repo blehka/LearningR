f507892991785c96e6398745fde42d217ac6edf7
Beata Joanna Lehka <blehka86@gmail.com>
2021-06-15 15:15:59 +0200

    Start of project

---
+++ R/project-session.R
@@ -0,0 +1,35 @@
+source(here::here("R/package-loading.R"))
+10
+
+# Basics of R -------------------------------------------------------------
+
+10
+weight_kilos <- 100
+weight_kilos
+C("a,"b,"c")
+c("a,"b"c")
+c("a", "b", "c")
+c(TRUE, FALSE)
+factor(c("high", "medium", "low"))
+head(CO2)
+colnames(CO2)
+str(CO2)
+summary(CO2)
+# Object names
+day_one
+T <- FALSE
+c <- 9
+
+# Spacing
+x[1]
+x[1]
+mean(x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+height <- feet * 12 + inches
+df$z
+x <- 1:10
+
+# Indenting and brackets
+if (y < 0 && debug) {
+    message("Y is negative")
+}
