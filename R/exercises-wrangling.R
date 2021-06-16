e0fdcdba39f4ed6633244a8bd450db7c50764ff2
Beata Joanna Lehka <blehka86@gmail.com>
2021-06-16 09:56:09 +0200

    checking data

---
+++ R/exercises-wrangling.R
@@ -0,0 +1,17 @@
+# Load the packages
+source(here::here("R/package-loading.R"))
+
+
+# Check column names
+colnames(NHANES)
+
+# Look at contents
+str(NHANES)
+glimpse(NHANES)
+
+# See summary
+summary(NHANES)
+
+# Look over the dataset documentation
+?NHANES
+
